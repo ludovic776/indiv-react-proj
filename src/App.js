0335acfccaecd571b187bcc1b5e684886f9108b2
Ludmila <nightmarelucy6@gmail.com>
2024-06-11 23:29:01 +0500

    added information about countries

--- src/App.js
+++ src/App.js
@@ -1,8 +1,9 @@
-// src/App.js
+// App.js
 
 import React, { useState } from 'react';
 import './style.css';
 import CurrencyConverter from './components/CurrencyConverter';
+import CountryInfo from './components/CountryInfo'; // Импортируем компонент
 
 function App() {
   const [fromCurrency, setFromCurrency] = useState('KZT');
@@ -29,6 +30,81 @@ function App() {
       element.scrollIntoView({ behavior: 'smooth' });
     }
   };
+
+  const countriesData = [
+    {
+      name: 'Казахстан',
+      flag: 'https://www.akorda.kz/assets/media/flag.jpg',
+      currencyCode: 'KZT',
+      currencySymbol: '₸',
+      currencyRate: '0.0023 USD',
+      capital: 'Астана',
+      population: '18,776,707',
+      region: 'Азия'
+    },
+    {
+      name: 'Россия',
+      flag: 'https://upload.wikimedia.org/wikipedia/commons/thumb/f/f3/Flag_of_Russia.svg/250px-Flag_of_Russia.svg.png',
+      currencyCode: 'RUB',
+      currencySymbol: '₽',
+      currencyRate: '0.0115 USD',
+      capital: 'Москва',
+      population: '146,599,183',
+      region: 'Европа'
+    },
+    {
+      name: 'США',
+      flag: 'https://upload.wikimedia.org/wikipedia/commons/thumb/a/a9/Flag_of_the_United_States_%28DoS_ECA_Color_Standard%29.svg/250px-Flag_of_the_United_States_%28DoS_ECA_Color_Standard%29.svg.png',
+      currencyCode: 'USD',
+      currencySymbol: '$',
+      currencyRate: '1 USD',
+      capital: 'Вашингтон',
+      population: '331,002,651',
+      region: 'Америка'
+    },
+    {
+      name: 'Китай',
+      flag: 'https://masterflag.ru/img/info-flagi/world/kitay.png',
+      currencyCode: 'CNY',
+      currencySymbol: '¥',
+      currencyRate: '0.16 USD',
+      capital: 'Пекин',
+      population: '1,439,323,776',
+      region: 'Азия'
+    },
+    {
+      name: 'Швейцария',
+      flag: 'https://flagof.ru/wp-content/uploads/2018/10/flagge-der-schweiz-schweizerfahne-hintergrundbilder-bestimmtfur-flagge-von-schweiz.jpg',
+      currencyCode: 'CHF',
+      currencySymbol: 'CHF',
+      currencyRate: '1.11 USD',
+      capital: 'Берн',
+      population: '8,654,622',
+      region: 'Европа'
+    },
+    {
+      name: 'Япония',
+      flag: 'https://upload.wikimedia.org/wikipedia/commons/9/9e/Flag_of_Japan.svg',
+      currencyCode: 'JPY',
+      currencySymbol: '¥',
+      currencyRate: '0.0091 USD',
+      capital: 'Токио',
+      population: '126,476,461',
+      region: 'Азия'
+    },
+    {
+      name: 'Чехия',
+      flag: 'https://upload.wikimedia.org/wikipedia/commons/c/cb/Flag_of_the_Czech_Republic.svg',
+      currencyCode: 'CZK',
+      currencySymbol: 'Kč',
+      currencyRate: '0.047 USD',
+      capital: 'Прага',
+      population: '10,708,981',
+      region: 'Европа'
+    },
+  ];
+
+  const [selectedCountry, setSelectedCountry] = useState(null);
 
   return (
     <div className="App">
@@ -63,8 +139,21 @@ function App() {
           </div>
         </div>
       </div>
+      <div className="app">
+        <h1>Информация о странах</h1>
+        <div className="country-list">
+          {countriesData.map((country, index) => (
+            <button key={index} onClick={() => setSelectedCountry(country)}>
+              <img src={country.flag} alt={country.name} style={{ width: '50px', height: 'auto' }} />
+              {country.name}
+            </button>
+          ))}
+        </div>
+        {selectedCountry && <CountryInfo country={selectedCountry} />}
+      </div>
     </div>
   );
 }
 
 export default App;
+
---
+++ src/components/CountryInfo.js
@@ -0,0 +1,24 @@
+// CountryInfo.js
+
+import React from 'react';
+import './CountryInfo.css';  
+
+const CountryInfo = ({ country }) => {
+  return (
+    <div className="country-info">
+      <h2>{country.name}</h2>
+      <img src={country.flag} alt={`Flag of ${country.name}`} style={{ width: '100px', height: 'auto' }} />
+      <div>
+        <p><strong>Код валюты:</strong> {country.currencyCode}</p>
+        <p><strong>Символ валюты:</strong> {country.currencySymbol}</p>
+        <p><strong>Официальный курс валюты:</strong> {country.currencyRate}</p>
+        <p><strong>Столица:</strong> {country.capital}</p>
+        <p><strong>Население:</strong> {country.population}</p>
+        <p><strong>Регион:</strong> {country.region}</p>
+        {/* Другие интересные факты о стране */}
+      </div>
+    </div>
+  );
+};
+
+export default CountryInfo;
